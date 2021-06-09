8cfd71cd65e38bb43575a8450738851ca1fd0822
malan88 <m@stdwtr.io>
2021-06-09 18:32:59 -0400

    Starting to make the US government

--- usa.py
+++ usa.py
@@ -1,16 +1,21 @@
 import random as r
-from institutions import Voter, Body, MultiHouse
+from institutions import *
 
 def main():
     # 4 bodies:
     #   1. People
-    people = Body('People', r.randint(1000,100000))
+    people = Body('People', r.randint(10000,100000))
+    states = []
+    i = 0
+
     #   2. Congress: House, Senate
     #   3. President
     #   4. SCOTUS
-    # congresses threshholds to be set by People
-    # president to be decided by people
-    # scotus thresholds to be set by president
+
+    # President to be chosen by people from people
+    # Senate to be chosen by states from states
+    # House to be chosen by districts from districts
+    # SCOTUS to be chosen by President
 
 
 if __name__ == '__main__':
